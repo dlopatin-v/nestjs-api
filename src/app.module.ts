6b927d143ea6b50d553d8a621d24d16eabe3ffa4
Denis Lopatin <stalk.sdv@gmail.com>
2020-12-02 19:03:05 +0100

    Removed root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
+import { Module } from '@nestjs/common'
+import { ProductsModule } from './products/products.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService],
+  imports: [ProductsModule]
 })
 export class AppModule {}
