8e5b4c2929b1d71ee826f1b147f33c1e4f867a1a
Denis Lopatin <stalk.sdv@gmail.com>
2020-12-02 19:13:42 +0100

    Added swagger descriptions to products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get } from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   @Get()
+  @ApiOperation({ summary: 'Get list of products' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   getAll(): string[] {
     return ['test']
   }
