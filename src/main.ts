ac887a5ffd53edaf69bd3906002f975ae55389bc
Denis Lopatin <stalk.sdv@gmail.com>
2020-12-03 19:07:35 +0100

    Cleaned

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,15 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
   const options = new DocumentBuilder()
     .setTitle('API example')
     .setDescription('API description')
     .setVersion('1.0')
     .addTag('products')
     .build()
-
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api', app, document)
-
   await app.listen(3000)
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap()
