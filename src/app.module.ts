aaa45dcfb6c58f0dd40a58a72d6aba83b322d74f
Denis Lopatin <stalk.sdv@gmail.com>
2020-12-08 23:59:48 +0100

    Added mongoose connection and nest configuration modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ProductsModule } from './products/products.module'
 
+const environment = process.env.NODE_ENV || 'development'
+
 @Module({
-  imports: [TypeOrmModule.forRoot(), ProductsModule]
+  imports: [
+    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_CONN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }),
+    ProductsModule
+  ]
 })
 export class AppModule {}
