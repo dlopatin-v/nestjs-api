b1ccf46517e25cb5b9d7c500bb0341ce95815687
Denis Lopatin <stalk.sdv@gmail.com>
2020-12-09 23:44:08 +0100

    Added user schema (mongodb). service methods to create and get users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ProductsModule } from './products/products.module'
+import { UserModule } from './user/user.module';
 
 const environment = process.env.NODE_ENV || 'development'
 
@@ -17,7 +18,8 @@ const environment = process.env.NODE_ENV || 'development'
       useNewUrlParser: true,
       useUnifiedTopology: true
     }),
-    ProductsModule
+    ProductsModule,
+    UserModule
   ]
 })
 export class AppModule {}
---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+export class CreateUserDTO {
+  readonly email: string
+  readonly avatar: string
+  readonly avatarId: string
+  readonly firstName: string
+  readonly lastName: string
+  readonly gender: string
+  readonly address: any
+  readonly profession: string
+  readonly phone: string
+  readonly roles: string[]
+  readonly password: string
+}
