fcc431dcd044b50543c74abcea1959b6a42ee03f
Denis Lopatin <stalk.sdv@gmail.com>
2021-01-05 16:41:53 +0100

    Added passport local and jwt strategies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { configModule } from 'configure.root'
 import { ProductsModule } from './products/products.module'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { AppController } from './app.controller'
 
 @Module({
   imports: [
@@ -13,7 +16,10 @@ import { ProductsModule } from './products/products.module'
       useUnifiedTopology: true,
       useCreateIndex: true
     }),
-    ProductsModule
-  ]
+    ProductsModule,
+    AuthModule,
+    UsersModule
+  ],
+  controllers: [AppController]
 })
 export class AppModule {}
